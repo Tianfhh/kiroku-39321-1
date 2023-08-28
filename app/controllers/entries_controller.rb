5946c2db75e822755d52c33fbf6fd55cea2b296e
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-28 21:22:21 +0900

    テータ保存前

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,5 @@
 class EntriesController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @entries = Entry.all
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :entries
+
   validates :name, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "must be a combination of letters and numbers" }
+
 end
