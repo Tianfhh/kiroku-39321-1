3b1e2092c8aef6e44c3c38a25a8df794933bc45f
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-28 16:42:07 +0900

    フロント修正-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "must be a combination of letters and numbers" }
 end
