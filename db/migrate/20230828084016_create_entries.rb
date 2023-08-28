871b352c62212535c300dc54a0991baf5e6a5a5a
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-28 17:49:03 +0900

    テーブルを作成

--- db/migrate/20230828084016_create_entries.rb
+++ db/migrate/20230828084016_create_entries.rb
@@ -2,6 +2,10 @@ class CreateEntries < ActiveRecord::Migration[6.0]
   def change
     create_table :entries do |t|
 
+      t.references :user, null: false, foreign_key: true
+      t.string :title, null: false
+      t.text :text, null: false
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_28_045030) do
+ActiveRecord::Schema.define(version: 2023_08_28_084016) do
+
+  create_table "entries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.text "text", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_entries_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2023_08_28_045030) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entries", "users"
 end
