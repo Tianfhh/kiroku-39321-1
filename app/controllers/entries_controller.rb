b3c9911b09d05facd1f17392a7e059cd9b5a87a5
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-29 12:43:28 +0900

    Revert "Update entries_controller.rb"
    This reverts commit 547bc52686c5169a9869f439e85028446643a02c.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,5 @@
 class EntriesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
 
   def index
     @entries = Entry.all
