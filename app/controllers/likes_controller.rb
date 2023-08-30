d5dffc4e6b2d9174ab97431bb3ceb870b1f88e47
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-30 11:28:05 +0900

    非同期処理の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,21 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :set_entry
+
   def create
     like = current_user.likes.build(entry_id: params[:entry_id])
     like.save
-    redirect_to root_path
+    render 'create.js.erb'
   end
 
   def destroy
     like = Like.find_by(entry_id: params[:entry_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    render 'destroy.js.erb'
+  end
+
+  def set_entry
+    @entry = Entry.find(params[:entry_id])
   end
 
 end
