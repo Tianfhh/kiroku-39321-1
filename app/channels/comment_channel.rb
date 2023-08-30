9078d9e14246f63a09f26c841b31a5d817bc7f8b
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-30 15:45:49 +0900

    チャネル変更

--- app/channels/comment_channel.rb
+++ app/channels/comment_channel.rb
@@ -1,6 +1,7 @@
 class CommentChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "comment_channel"
+    @entry = Entry.find(params[:entry_id])
+    stream_for @entry
   end
 
   def unsubscribed
