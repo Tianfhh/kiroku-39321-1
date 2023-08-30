3d412b83b7b78fcd0db28669e8123ae0f78ddcef
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-30 14:21:33 +0900

    commentsコントローラー編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to entry_path(params[:entry_id])
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, entry_id: params[:entry_id])
+  end
+
 end
