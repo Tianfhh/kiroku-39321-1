3a734228debf72d1ae62eaeb46dae6fbfaf8c7bc
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-30 15:48:27 +0900

    comment_channel.js編集

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -2,7 +2,11 @@ import consumer from "./consumer"
 
 if(location.pathname.match(/\/entries\/\d/)){
 
-  consumer.subscriptions.create("CommentChannel", {
+  cconsumer.subscriptions.create({
+    channel: "CommentChannel",
+    entry_id: location.pathname.match(/\d+/)[0]
+  }, {
+
   connected() {
     // Called when the subscription is ready for use on the server
   },
