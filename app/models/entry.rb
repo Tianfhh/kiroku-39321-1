2e1ccfaaa782137c02860ce8661600bbf2c37eba
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-09-07 20:15:38 +0900

    親モデルが削除されると子モデルも一緒に削除されること

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -6,8 +6,8 @@ class Entry < ApplicationRecord
   
 
   belongs_to :user
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   
   has_one_attached :image
 
