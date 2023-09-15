35d471cad2cae04945e63797d1915736b9272bd5
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-09-15 11:59:20 +0900

    Revert "テストコードの一部修正-1"
    This reverts commit 0fb3e52e647d232958299f5ba96cdf54b2a4cd9e.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,37 +14,37 @@ RSpec.describe User, type: :model do
      end
 
      context '内容に問題がある場合' do
-      it 'ネームが空では登録できない' do
+      it 'nameが空では登録できない' do
         @user.name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include "ネームを入力してください"
       end
   
-      it "Eメールが空では登録できない" do
+      it "emailが空では登録できない" do
         @user.email = ''  
         @user.valid?
         expect(@user.errors.full_messages).to include "Eメールを入力してください"
       end
   
-      it "パスワードが空だと登録できない" do
+      it "passwordが空だと登録できない" do
         @user.password = ''  
         @user.valid?
         expect(@user.errors.full_messages).to include "パスワードを入力してください"
       end
   
-      it "パスワードが英字のみでは登録できない" do
+      it "passwordが英字のみでは登録できない" do
         @user.password = 'aaaaaaa'
         @user.valid?
         expect(@user.errors.full_messages).to include "パスワードは英数字混合で入力してください"
       end
   
-      it "パスワードが数字のみでは登録できない" do
+      it "passwordが数字のみでは登録できない" do
         @user.password = '0000000'
         @user.valid?
         expect(@user.errors.full_messages).to include "パスワードは英数字混合で入力してください"
       end
   
-      it "パスワードが全角文字を含む場合は登録できない" do
+      it "passwordが全角文字を含む場合は登録できない" do
         @user.password = 'aaa０00' 
         @user.valid?
         expect(@user.errors.full_messages).to include "パスワードは英数字混合で入力してください"
